72f18575f843d458c71b640b73352990636d4e6d
sofoklisfountas <sofoklisfountas@gmail.com>
2022-11-07 00:52:14 +0200

    removed .container

--- dist/competitors.0f6fc479.js
+++ dist/competitors.0f6fc479.js
@@ -237,7 +237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55646" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49927" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
